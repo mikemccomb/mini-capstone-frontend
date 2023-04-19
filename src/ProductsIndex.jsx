b8057bef22add08e516d2f2bd7f8bdaf6ee22695
mikemccomb <mikemccomb@gmail.com>
2023-04-19 13:38:37 -0500

    Adds images to Products Index

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -3,7 +3,11 @@ export function ProductsIndex(props) {
     <div>
       <h2>These are Products</h2>
       {props.products.map((product) => (
-        <div key={product.id}>{product.name}</div>
+        <div key={product.id}>
+          <h3>{product.name}</h3>
+          <img src={product.images[0].url} alt={product.name} />
+          <p>{product.description}</p>
+        </div>
       ))}
     </div>
   );
